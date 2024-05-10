606e4d6ca09e3dc0c133b3a2eb4c47fd7eb1d634
Maddy <magdalini.palaiologou@siemens.com>
2024-05-10 21:15:07 +0200

    rating component

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-star-rating',
   standalone: true,
   imports: [],
   templateUrl: './star-rating.component.html',
-  styleUrl: './star-rating.component.scss'
+  styleUrl: './star-rating.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: StarRatingComponent,
+    },
+  ]
 })
-export class StarRatingComponent {
+export class StarRatingComponent implements ControlValueAccessor {
+  @Input() value: number;
+  @Input() disabled: boolean = false;
 
+  onChange = (value) => {};
+  onTouched = () => {};
+  protected touched = false;
+
+  valueChanged(): void {
+    this.onChange(this.value);
+  }
+
+  rate(rating: number): void {
+    this.markAsTouched();
+    if (!this.disabled) {
+      this.value = rating;
+      this.onChange(this.value);
+    }
+  }
+
+  writeValue(value: number) {
+    this.value = value;
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+  }
 }
